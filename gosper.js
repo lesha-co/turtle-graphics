2411f2f777f5a6fa4e6197be94d51ff8b865ec70
Aleksei Kuzmichev <lesha@lesha.co>
2025-06-17 00:30:30 +0200

    npm

--- gosper.js
+++ gosper.js
@@ -1,24 +1,26 @@
 import { TurtleManager } from "./turtle.js";
 
 const turtleManager = new TurtleManager("canvas");
+turtleManager.initCanvas();
 
 turtleManager.ctx.fillStyle = "#000"; // e.g., "#000000"
 turtleManager.ctx.fillRect(
   0,
   0,
   turtleManager.canvas.width,
-  turtleManager.canvas.height,
+  turtleManager.canvas.height
 );
 
-const turtle = turtleManager.spawn(500, 250);
+const turtle = turtleManager.spawn(400, 0);
 turtle.setSpeed(1000);
+turtle.setWidth(1);
 turtle.hide();
 
 let hue = 0;
 async function step(turtle) {
   hue++;
   turtle.setColor(`hsl(${hue / 150}, 100%, 50%)`);
-  await turtle.draw(2);
+  await turtle.draw(10);
 }
 
 turtle.L_System(
@@ -27,12 +29,12 @@ turtle.L_System(
     A: "A-B--B+A++AA+B-",
     B: "+A-BB--B-A++A+B",
   },
-  6,
+  5,
   {
     // commands
     "+": (turtle) => turtle.rotate(-60),
     "-": (turtle) => turtle.rotate(60),
     A: step,
     B: step,
-  },
+  }
 );
